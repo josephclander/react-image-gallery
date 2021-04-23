e16e72f246e9e7e5cda83af993814e0f7793c305
Joseph Lander <josephclander@gmail.com>
2021-04-23 14:23:23 +0100

    Add basic flickr search for raw data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
+import React, { Component } from 'react';
+
 import Search from './Components/Search';
 import Nav from './Components/Nav';
 import PhotoContainer from './Components/PhotoContainer';
+import axios from 'axios';
+import apiKey from './config';
 
-function App() {
-  return (
-    <div className='container'>
-      <Search />
-      <Nav />
-      <PhotoContainer />
-    </div>
-  );
+const searchFlickr = (query = 'cats') => {
+  axios
+    .get(
+      `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${query}&per_page=24&page=1&format=json&nojsoncallback=1`
+    )
+    .then((data) => console.log(data.data.photos.photo))
+    .catch((error) => {
+      console.log('Error fetching and parsing data', error);
+    });
+};
+searchFlickr();
+
+class App extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <Search />
+        <Nav />
+        <PhotoContainer />
+      </div>
+    );
+  }
 }
 
 export default App;

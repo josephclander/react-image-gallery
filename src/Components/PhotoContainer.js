8e6e9cdb25907fc5840d127c9ae7147348eab7d2
Joseph Lander <josephclander@gmail.com>
2021-05-03 18:17:43 +0100

    Add no matches msg to search results

--- src/Components/PhotoContainer.js
+++ src/Components/PhotoContainer.js
@@ -7,11 +7,14 @@ const PhotoContainer = (props) => {
     <div className='photo-container'>
       <h2>{props.title}</h2>
       <ul>
-        {output}
-        {/* <li className='not-found'>
-          <h3>No Results Found</h3>
-          <p>You search did not return any results. Please try again.</p>
-        </li> */}
+        {props.data.length > 0 ? (
+          output
+        ) : (
+          <li className='not-found'>
+            <h3>No Results Found</h3>
+            <p>You search did not return any results. Please try again.</p>
+          </li>
+        )}
       </ul>
     </div>
   );
